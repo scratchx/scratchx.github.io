e7857a0814383f37f46d38ee89636a0ef1dfb2c4
kooll <kishi@tsuda.ac.jp>
2016-07-23 22:43:28 +0900

    Add one more javascript

---
+++ js/SetOne.js
@@ -0,0 +1,13 @@
+// Set One for experimental extension blocks for scrachx workshop
+// twitter
+ScratchExtensions.loadExternalJS("https://technoboy10.github.io/twitter/extension.js");
+// RSS reader:
+ScratchExtensions.loadExternalJS("http://savaka2.github.io/scratch-extensions-directory/extensions/FeedMe_RSS_fun.js");
+// text to speech: needs windows
+ScratchExtensions.loadExternalJS("http://sayamindu.github.io/scratch-extensions/text-to-speech/text_to_speech_extension.js");
+// kinect v1 suport: needs windows and kinect v1
+ScratchExtensions.loadExternalJS("http://stephenhowell.github.io/Kinect2ScratchX.js");
+// extension Loader
+ScratchExtensions.loadExternalJS("http://thepuzzlegame.github.io/extensions/Extension_Loader.js");
+// end
+
